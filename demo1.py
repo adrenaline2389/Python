ed2e2e2942bcd20a8a2bd6e189cccd56388efe48
adrenaline <2728407164@qq.com>
2024-08-25 14:58:05 +0800

    optimize: 操作优化

--- demo1.py
+++ demo1.py
@@ -3,23 +3,11 @@ x,y,z="石头","剪刀","布"
 player=input("请输入:")
 computer=z
 
-if player!=computer:
-    if player == x:
-        if computer == y:
-            print("赢")
-        if computer == z:
-            print("输")
-    elif player == y:
-        if computer == x:
-            print("输")
-        if computer == z:
-            print("赢")
-    else:
-        if computer == x:
-            print("赢")
-        if computer == y:
-            print("输")
+if (player==x and computer==y) or (player==y and computer==z)or (player==z and computer==x):
+    print('win')
+elif player==computer:
+    print('peace')
 else :
-    print('平')
+    print('lose')
 
 

fdd66d441cdc10e0effa47d148ea6ab7a760c0cc
adrenaline <2728407164@qq.com>
2024-08-25 15:18:12 +0800

    optimize: 操作优化(circulation)

--- demo1.py
+++ demo1.py
@@ -1,13 +1,18 @@
 #先给出3个变量，再获得用户的输入，电脑再从三个变量中随机选择一个
 x,y,z="石头","剪刀","布"
-player=input("请输入:")
-computer=z
-
-if (player==x and computer==y) or (player==y and computer==z)or (player==z and computer==x):
-    print('win')
-elif player==computer:
-    print('peace')
-else :
-    print('lose')
+# for i in range(3):
+i = 0
+while i <=2:
 
 
+    player=input("请输入:")
+    computer=z
+
+    if (player == x and computer == y) or (player == y and computer == z) or (player == z and computer == x):
+        print('win')
+    elif player==computer:
+        print('peace')
+    else :
+        print('lose')
+
+    i = i + 1

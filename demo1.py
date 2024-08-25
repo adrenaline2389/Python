64a6d60b4b71caf5502cc12fd7c56f8889ec3951
adrenaline <2728407164@qq.com>
2024-08-25 17:13:05 +0800

    optimize: 操作优化(random取整) bug：if并列关系未考虑好，不是数字就直接报错，而不是继续判断。 bug：概率-要放在循环内 bug：= 和 == 的区别 advantage：概率直接取整，可以直接取数 advantage：快速找到自己的问题，能找到优化的点，随机出拳，策略出拳。

--- demo1.py
+++ demo1.py
@@ -1,4 +1,17 @@
-#先给出3个变量，再获得用户的输入，电脑再从三个变量中随机选择一个
+# 伪代码：先给出3个变量，再获得用户的输入，电脑再从三个变量中随机选择一个
+
+# 任务
+# 1. 设计一个石头剪刀布游戏
+# 2. input， if， 循环 break continue， = ==， and or的用法，字典和列表
+# 3. if的优化（代码的重构）程序的健壮性 非法输入的处理
+# 4. 自己想的优化点 电脑随机出拳（advantage：等概率直接取整，可以直接取数。
+
+# 课后作业
+# 电脑跟据我出拳的招式优化自己的策略
+
+# 总结
+# 快速找到自己的问题，能找到优化的点，随机出拳，策略出拳。
+# 不要给自己设限制。
 import random
 
 x,y,z="石头","剪刀","布"
@@ -11,19 +24,19 @@ x,y,z="石头","剪刀","布"
 # }
 a =["石头","剪刀","布","exit"]
 
-# 设计电脑出的招式是等概率的
-p = random.random()*3
+
+p = random.random() * 3
 q = int(p)
 # for i in range(3):
 i = 0
-while i <= 10:
+while i <= 100:
     player_input = input("请输入  " + str(a) + ":\n")
     if not player_input.isdigit():
         print('ERROR')
         continue
     else:
         player_input = int(player_input)
-        if player_input <= 3 and player_input >= 0:
+        if 0 <= player_input <= 3:
             if a[player_input] == "exit":
                 print("end")
                 break
@@ -31,12 +44,7 @@ while i <= 10:
             player = a[player_input]
             p = random.random() * 3
             q = int(p)
-            if q == 0:
-                computer = x
-            if q == 1:
-                computer = y
-            if q == 2:
-                computer = z
+            computer = a[q]
 
             if (player == x and computer == y) or (player == y and computer == z) or (
                     player == z and computer == x):
@@ -47,5 +55,5 @@ while i <= 10:
                 print("我出: " + player, "电脑出: " + computer, "结果是: lose")
 
             i = i + 1
-        else:
+        else :
             print('ERROR')

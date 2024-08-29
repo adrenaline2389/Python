fdf8511e9220a9852628a91a6e0ce263c14bb152
adrenaline <2728407164@qq.com>
2024-08-30 01:17:24 +0800

    feature: 第三章 列表简介

---
+++ 0830/3.List.py
@@ -0,0 +1,37 @@
+# 第三章
+A = []
+
+
+# # 增
+A.append('alpha')
+A.append('gamma')
+print(A)
+
+A.insert(1,'beta')
+A.insert(3,'delta')
+A.insert(4,'epsilon')
+print(A)
+
+
+# # 删
+del A[4]
+print(A)
+
+A.pop()
+print(A)
+
+A.remove('gamma')
+print(A)
+
+
+# # 查
+print(len(A))
+
+
+# # 改
+A.sort(reverse = True)
+print(A)
+
+A = sorted(A,reverse=True)
+print(A)
+

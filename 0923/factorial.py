27f36b2ccdcf9429c90d64edc0d216b86767b579
adrenaline2389 <2728407164@qq.com>
2024-09-24 17:16:26 +0800

    Add files via upload

--- 0923/factorial.py
+++ 0923/factorial.py
@@ -1,10 +1,19 @@
-# 递归实现  不理解!!!
-def factorial(n):
+# 递归&迭代实现
+def factorial_1(n):
     if n == 0:
         return 1
     count = 0
     for i in range(n):
-        count += factorial(n - 1)
+        count += factorial_1(n - 1)
     return count
 
-print(factorial(6))
+print(factorial_1(6))
+
+
+# ChatGPT方法(recursion only）
+def factorial_2(n):
+    if n == 0:
+        return 1
+    return n * factorial_2(n - 1)
+
+print(factorial_2(6))
